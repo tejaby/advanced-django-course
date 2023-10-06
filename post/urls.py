e6f07eea72fe32cd0d0ffea06ec2c254e2a4054d
tejaby <yostintejaxun20@gmail.com>
2023-10-06 00:48:58 -0600

    add views CreateView, UpdateView, DeleteView class-based

--- post/urls.py
+++ post/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import home, postsListView, create_user, update_user
+from .views import home, UserListView, UserCreateView, UserUpdateView, UserDeleteView
 
 urlpatterns = [
     path('', home.as_view(), name='home'),
-    path('list_posts/', postsListView.as_view(), name='list_posts'),
-    path('create_user/', create_user, name='create_user'),
-    path('update_user/<int:user_id>/', update_user, name='update_user'),
+    path('list_user/', UserListView.as_view(), name='list_user'),
+    # path('create_user/', create_user, name='create_user'),
+    # path('update_user/<int:user_id>/', update_user, name='update_user'),
+    path('create_user', UserCreateView.as_view(), name='create_user'),
+    path('update_user/<int:pk>/', UserUpdateView.as_view(), name='update_user'),
+    path('delete_user/<int:pk>/', UserDeleteView.as_view(), name='delete_user')
 ]
--- post/views.py
+++ post/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
+from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import View, TemplateView, ListView, UpdateView
+from django.views.generic import View, TemplateView, ListView, CreateView, UpdateView, DeleteView
 
 from .models import Post, User
 
@@ -12,29 +13,52 @@ from .forms import UserForm
 class home(TemplateView):
     template_name = 'index.html'
 
-class postsListView(ListView):
-    model = Post
-    template_name = 'post/list_post.html'
+class UserListView(ListView):
+    model = User
+    template_name = 'post/list_user.html'
+    queryset = User.objects.filter(state=True)
 
-def create_user(request):
-    if request.method == 'POST':
-        fomr_user = UserForm(request.POST)
-        if fomr_user.is_valid():
-            fomr_user.save()
-        return redirect('home')
-    else:
-        return render(request, 'post/create_user.html', {'form': UserForm})
+class UserCreateView(CreateView):
+    model = User
+    template_name = 'post/create_user.html'
+    form_class = UserForm
+    success_url = reverse_lazy('list_user')
+
+class UserUpdateView(UpdateView):
+    model = User
+    template_name = 'post/create_user.html'
+    form_class = UserForm
+    success_url = reverse_lazy('list_user')
+
+class UserDeleteView(DeleteView):
+    model = User
+    success_url = reverse_lazy('list_user')
+
+    def post(self, request, pk, *args, **kwargs):
+        object = User.objects.get(id=pk)
+        object.state = False
+        object.save()
+        return redirect('list_user')
+
+# def create_user(request):
+#     if request.method == 'POST':
+#         fomr_user = UserForm(request.POST)
+#         if fomr_user.is_valid():
+#             fomr_user.save()
+#         return redirect('home')
+#     else:
+#         return render(request, 'post/create_user.html', {'form': UserForm})
     
-def update_user(request, user_id):
-    try:
-        user =get_object_or_404(User, id=user_id)
-    except User.DoesNotExist:
-        return HttpResponse("user not found", status=404)
-    if request.method == 'POST':
-        form_user = UserForm(request.POST, initial=user)
-        if form_user.is_valid():
-            form_user.save()
-        return redirect('home')
-    else:
-        form = UserForm(instance=user)
-        return render(request, 'post/create_user.html', {'form': form})
+# def update_user(request, user_id):
+#     try:
+#         user = get_object_or_404(User, id=user_id)
+#     except User.DoesNotExist:
+#         return HttpResponse("user not found", status=404)
+#     if request.method == 'POST':
+#         form_user = UserForm(request.POST, initial=user)
+#         if form_user.is_valid():
+#             form_user.save()
+#         return redirect('home')
+#     else:
+#         form = UserForm(instance=user)
+#         return render(request, 'post/create_user.html', {'form': form})
